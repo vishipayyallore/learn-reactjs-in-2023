bbb2ed3118f9f5fd5c4fedf14b3d2fcd2d678301
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-04-10 17:54:29 +0530

    Update SignIn.jsx

--- realestates/src/pages/SignIn.jsx
+++ realestates/src/pages/SignIn.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function SignIn() {
 
@@ -47,6 +47,30 @@ export default function SignIn() {
                                     onClick={() => setShowPassword((prevState) => !prevState)}
                                 />
                             )}
+                        </div>
+
+                        <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+                            <p className="mb-6">
+                                Don't have a account?
+                                <Link to="/sign-up" className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1" >
+                                    Register
+                                </Link>
+                            </p>
+                            <p>
+                                <Link to="/forgot-password" className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out">
+                                    Forgot password?
+                                </Link>
+                            </p>
+                        </div>
+
+                        <button
+                            className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+                            type="submit"
+                        >
+                            Sign in
+                        </button>
+                        <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+                            <p className="text-center font-semibold mx-4">OR</p>
                         </div>
                     </form>
                 </div>
