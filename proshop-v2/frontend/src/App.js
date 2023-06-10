048fdde7351cbb7ddcdbd7d69741747ef858af48
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-10 22:29:21 +0530

    Added Footer

--- proshop-v2/frontend/src/App.js
+++ proshop-v2/frontend/src/App.js
@@ -1,6 +1,6 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
-
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
@@ -10,6 +10,7 @@ const App = () => {
           <h1>ProShop</h1>
         </Container>
       </main>
+      <Footer />
     </>
   );
 };
