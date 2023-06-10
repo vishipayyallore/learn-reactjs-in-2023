80bec8b7946bd26762deaf332cad9bb35cd19088
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-11 00:25:52 +0530

    Swamy/10jun work (#7)
    * Initial Version
    
    * New Project
    
    * Initial Version
    
    * Changes
    
    * Changes
    
    * Header Comopnent
    
    * Added Footer

---
+++ proshop-v2/frontend/src/App.js
@@ -0,0 +1,18 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ proshop-v2/readme.md
@@ -0,0 +1,11 @@
+# ProShop eCommerce Platform
+
+> eCommerce platform built with the MERN stack & Redux.
+
+## Commands
+
+```bash
+npx create-react-app frontend
+
+npm i react-bootstrap bootstrap react-icons
+```
