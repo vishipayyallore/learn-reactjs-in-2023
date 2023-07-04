38d65488083039edc0d5d486a7b793d67835625b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-04 23:44:23 +0530

    Update App.js

--- proshop-v2/frontend/src/App.js
+++ proshop-v2/frontend/src/App.js
@@ -1,14 +1,16 @@
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
+
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
