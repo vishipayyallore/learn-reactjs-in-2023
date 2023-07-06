d3384b05a7f99afe20ba8ae81ed738f5e58cd286
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-06 11:37:44 +0530

    Swamy/04jul react router day1 (#10)
    * React Router
    
    * Update index.js
    
    * Update App.js
    
    * Update Product.jsx
    
    * Client Side Routing
    
    * New Packages
    
    * Create Rating.jsx
    
    * Update index.css
    
    * Update Product.jsx
    
    * Update Product.jsx

--- proshop-v2/frontend/src/components/Product.jsx
+++ proshop-v2/frontend/src/components/Product.jsx
@@ -1,6 +1,8 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+import Rating from "./Rating";
+
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
@@ -10,10 +12,14 @@ const Product = ({ product }) => {
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+
+        <Card.Text as="div">
+          <Rating value={product.rating} text={`${product.numReviews} Reviews`} />
+        </Card.Text>
 
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
