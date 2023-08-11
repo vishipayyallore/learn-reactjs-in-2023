a701682f9a06ce00c3d57c725490beaa36072068
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-11 14:34:44 +0530

    Update TodoItems.jsx

--- todo-app/src/screens/TodoItems.jsx
+++ todo-app/src/screens/TodoItems.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 
-export default function TodoItems(props) {
-  const entries = props.entries;
-  const deleteItem = props.deleteItem;
-
+export default function TodoItems({ entries, deleteItem }) {
   return (
     <ul className="mt-3">
       <b className="ms-3">Items:</b>
@@ -23,3 +20,27 @@ export default function TodoItems(props) {
     </ul>
   );
 }
+
+// export default function TodoItems(props) {
+//   const entries = props.entries;
+//   const deleteItem = props.deleteItem;
+
+//   return (
+//     <ul className="mt-3">
+//       <b className="ms-3">Items:</b>
+//       {entries.map(({ id, text }) => {
+//         return (
+//           <li
+//             className="px-2 mt-1 mb-1 ms-2"
+//             key={id}
+//             onClick={() => {
+//               deleteItem(id);
+//             }}
+//           >
+//             {text}
+//           </li>
+//         );
+//       })}
+//     </ul>
+//   );
+// }
