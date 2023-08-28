fef1d6de473ae1bfb05dd5bda1e201825cfc9ed3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-28 07:11:47 +0530

    Swamy/27aug work (#13)
    * Initial Version
    
    * Changes
    
    * Initial Version
    
    * Changes
    
    * Update _app.jsx
    
    * Changes
    
    * Changes
    
    * Create launch.json
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes

---
+++ globomantics/components/banner.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import { logo } from "./banner.module.css";
+
+const subtitleStyle = {
+  fontStyle: "italic",
+  fontSize: "x-large",
+  color: "coral",
+};
+
+const Banner = ({ children }) => {
+  return (
+    <header className="row mb-4">
+      <div className="col-5">
+        <Image src="/GloboLogo.png" alt="logo" className={logo} width={200} height={150} />
+      </div>
+      <div className="col-7 mt-5" style={subtitleStyle}>
+        {children}
+      </div>
+    </header>
+  );
+};
+
+export default Banner;
---
+++ globomantics/pages/index.jsx
@@ -0,0 +1,11 @@
+import App from "@/components/app";
+
+const Index = () => {
+  return (
+    <div>
+      <App />
+    </div>
+  );
+};
+
+export default Index;
