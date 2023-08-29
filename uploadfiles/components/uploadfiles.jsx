bf512fcdab12b616b0c571cc509ff79cbe92d037
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-29 18:29:48 +0530

    Changes

--- uploadfiles/components/uploadfiles.jsx
+++ uploadfiles/components/uploadfiles.jsx
@@ -1,33 +1,44 @@
 import React, { useState } from 'react';
 
 const UploadFiles = () => {
-    const [files, setFiles] = useState({
-        image: null,
-        textFile1: null,
-        textFile2: null,
-    });
+    const [image, setImage] = useState('');
+    const [file1, setFile1] = useState('');
+    const [file2, setFile2] = useState('');
 
-    const handleFileChange = (event, fileType) => {
+    const handleFileChange = (event, setFile) => {
         const selectedFile = event.target.files[0];
-        setFiles((prevFiles) => ({
-            ...prevFiles,
-            [fileType]: selectedFile,
-        }));
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            setFile(e.target.result);
+        };
+
+        reader.readAsDataURL(selectedFile);
     };
 
-    const handleUpload = () => {
-        // You can implement the file upload logic here
-        // For simplicity, this example just logs the selected files
-        console.log('Selected Files:', files);
+    const handleUpload = async () => {
+        const response = await fetch('/api/upload', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ image, file1, file2 }),
+        });
+
+        if (response.ok) {
+            console.log('Files uploaded successfully');
+        } else {
+            console.error('An error occurred while uploading files');
+        }
     };
 
     return (
         <div className="container border border-primary rounded-2 p-2">
             <h1>Upload Files</h1>
             <div>
-                <input type="file" accept=".png" onChange={(e) => handleFileChange(e, 'image')} />
-                <input type="file" accept=".txt" onChange={(e) => handleFileChange(e, 'textFile1')} />
-                <input type="file" accept=".txt" onChange={(e) => handleFileChange(e, 'textFile2')} />
+                <input type="file" accept=".png" onChange={(e) => handleFileChange(e, setImage)} />
+                <input type="file" accept=".txt" onChange={(e) => handleFileChange(e, setFile1)} />
+                <input type="file" accept=".txt" onChange={(e) => handleFileChange(e, setFile2)} />
             </div>
             <button onClick={handleUpload}>Upload</button>
         </div>
