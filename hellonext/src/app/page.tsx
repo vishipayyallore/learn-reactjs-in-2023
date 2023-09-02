dff3570b3be2a24a9125f7d74216219790608ba3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-02 11:21:28 +0530

    Update page.tsx

--- hellonext/src/app/page.tsx
+++ hellonext/src/app/page.tsx
@@ -10,11 +10,14 @@ export default function Home() {
           </div>
 
           <div className='flex gap-5'>
-          <Link href='/projects'>Projects</Link>
-          <Link href='/resume'>Resume</Link>
-          <Link href='/contactus'>Contact Us</Link>
+            <Link href='/projects'>Projects</Link>
+            <Link href='/resume'>Resume</Link>
+            <Link href='/contactus'>Contact Us</Link>
           </div>
         </div>
+      </div>
+      <div className='flex justify-center p-5'>
+        <h1 className='font-semibold'>Welcome to my website</h1>
       </div>
     </>
   )
