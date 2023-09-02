0d755b86fe27ef1ecdbc19ec0203e4de2fe8d06e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-02 12:12:28 +0530

    Create page.tsx

---
+++ hellonext/src/app/projects/[projectid]/reviews/page.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Link from "next/link";
+
+function ProjectInfo({ params }: any) {
+
+    const { projectid } = params;
+
+    return (
+        <>
+            <div>
+                <div className='flex justify-left p-5 bg-gray-700 text-white gap-5'>
+                    <div>
+                        <h1 className='font-semibold'>Swamy PKV</h1>
+                    </div>
+
+                    <div className='flex gap-5'>
+                        <Link href='/projects'>Projects</Link>
+                        <Link href='/resume'>Resume</Link>
+                        <Link href='/contactus'>Contact Us</Link>
+                    </div>
+                </div>
+
+                <h1 className="text-2xl text-center">Project Details of - {projectid}</h1>
+
+                <div className="flex gap-10 text-xl p-5">
+                    <Link href='/projects'>Back to Projects</Link>
+
+                    <Link href={`/projects/${projectid}/reviews`}>Current Project Reviews</Link>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default ProjectInfo;
