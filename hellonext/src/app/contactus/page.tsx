6c7d4690b1d2d1b75ad04c151ea049c788a1a013
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-02 11:03:33 +0530

    Create page.tsx

---
+++ hellonext/src/app/contactus/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ContactUs() {
+    return (
+        <>
+            <div>
+                <h1 className="text-2xl">Contact Us</h1>
+            </div>
+        </>
+    );
+}
+
+export default ContactUs;
