385df7be8d9c857e5bb9d83af730d6f4b7432e43
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-02 11:01:40 +0530

    Create page.tsx

---
+++ hellonext/src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Projects() {
+    return (
+        <>
+            <div>
+                <h1 className="text-2xl">Projects</h1>
+            </div>
+        </>
+    );
+}
+
+export default Projects;
