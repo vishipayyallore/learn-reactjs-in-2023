76faaa89feed06c02345c167282bff4276bd954c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-02 11:57:43 +0530

    Update page.tsx

--- hellonext/src/app/projects/[projectid]/page.tsx
+++ hellonext/src/app/projects/[projectid]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
-function Projects() {
+function ProjectInfo() {
     return (
         <>
             <div>
@@ -17,42 +17,12 @@ function Projects() {
                     </div>
                 </div>
 
-                <h1 className="text-2xl text-center">Current Projects</h1>
+                <h1 className="text-2xl text-center">Project Details of - </h1>
 
-                <div className="flex justify-center gap-10 p-10">
-                    <Link href='/projects/1'>
-                        <div className="h-52 w-52 bg-gray-100">
-                            <h1 className="text-2xl text-center">Project 1</h1>
-                        </div>
-                    </Link>
 
-                    <Link href='/projects/2'>
-                        <div className="h-52 w-52 bg-gray-200">
-                            <h1 className="text-2xl text-center">Project 2</h1>
-                        </div>
-                    </Link>
-
-                    <Link href='/projects/3'>
-                        <div className="h-52 w-52 bg-gray-300">
-                            <h1 className="text-2xl text-center">Project 3</h1>
-                        </div>
-                    </Link>
-
-                    <Link href='/projects/4'>
-                        <div className="h-52 w-52 bg-gray-400">
-                            <h1 className="text-2xl text-center">Project 4</h1>
-                        </div>
-                    </Link>
-
-                    <Link href='/projects/5'>
-                        <div className="h-52 w-52 bg-gray-500">
-                            <h1 className="text-2xl text-center">Project 5</h1>
-                        </div>
-                    </Link>
-                </div>
             </div>
         </>
     );
 }
 
-export default Projects;
+export default ProjectInfo;
