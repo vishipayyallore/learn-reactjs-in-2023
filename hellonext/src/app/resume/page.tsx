ca8c4c32f774f7896d84a2884ace5d2c309c6980
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-02 11:07:55 +0530

    Create page.tsx

---
+++ hellonext/src/app/resume/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Resume() {
+    return (
+        <>
+            <div>
+                <h1 className="text-2xl">Resume</h1>
+            </div>
+        </>
+    );
+}
+
+export default Resume;
