77fc4e0c111c4befb1fb7ee86d165e0b4756751d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-02 11:17:34 +0530

    Update page.tsx

--- hellonext/src/app/page.tsx
+++ hellonext/src/app/page.tsx
@@ -1,10 +1,20 @@
-import Image from 'next/image'
+import Link from "next/link"
 
 export default function Home() {
   return (
     <>
       <div>
-        <h1>Hello Next JS App!</h1>
+        <div className='flex justify-left p-5 bg-gray-700 text-white gap-5'>
+          <div>
+            <h1 className='font-semibold'>Swamy PKV</h1>
+          </div>
+
+          <div className='flex gap-5'>
+          <Link href='/projects'>Projects</Link>
+          <Link href='/resume'>Resume</Link>
+          <Link href='/contactus'>Contact Us</Link>
+          </div>
+        </div>
       </div>
     </>
   )
