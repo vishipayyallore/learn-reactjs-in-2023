a91d6967ee1adc7fa9e4f3b5f8dca6487fa631bf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-03 14:28:10 +0530

    Changes

--- ecomm/src/components/shared/header.tsx
+++ ecomm/src/components/shared/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Header = () => {
     return (
         <div className='flex justify-between p-5 text-white gap-5 items-center h-6 bg-gradient-to-r from-blue-600 to-blue-900'>
-            <Link className="text-2xl font-bold" href='/'>eCommerece</Link>
+            <Link className="text-2xl font-bold" href='/'>eCommerce</Link>
 
             <div className='flex gap-5'>
                 <Link href='/orders'>Orders</Link>
