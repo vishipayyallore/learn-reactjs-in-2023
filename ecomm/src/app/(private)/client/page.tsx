79401b033d608fb7a4eb002a8e30ee967d55836d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-04 15:51:38 +0530

    Changes

---
+++ ecomm/src/app/(private)/client/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from "react";
+
+const ClientPage = () => {
+  return (
+    <>
+      <div>
+        <h1>Client Component</h1>
+      </div>
+    </>
+  )
+};
+
+export default ClientPage;
---
+++ ecomm/src/app/(private)/server/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ServerPage = () => {
+  return (
+    <>
+      <div>
+        <h1>Server Component</h1>
+      </div>
+    </>
+  )
+};
+
+export default ServerPage;
