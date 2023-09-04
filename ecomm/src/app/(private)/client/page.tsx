f7fd5440bedeae6e8a0ac5630db19208856f0972
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-04 16:03:05 +0530

    Update page.tsx

--- ecomm/src/app/(private)/client/page.tsx
+++ ecomm/src/app/(private)/client/page.tsx
@@ -3,10 +3,41 @@
 import React from "react";
 
 const ClientPage = () => {
+
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    try {
+      await fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => setUsers(data))
+    } catch (error: any) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <div>
-        <h1>Client Component</h1>
+        <h1 className="text-4xl font-bold text-center text-cyan-900">Client Component</h1>
+        <hr></hr>
+
+        <div>
+          <ul>
+            {users.map((user: any) => (
+              <li key={user.id}>{user.name}</li>
+            ))}
+          </ul>
+        </div>
+
+        <hr></hr>
+        <p>
+          {JSON.stringify(users)}
+        </p>
       </div>
     </>
   )
