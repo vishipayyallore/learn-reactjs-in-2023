1437695ef7f4e59ac3a71b0d7be9e0a104e4d510
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-08 18:23:36 +0530

    Changes

---
+++ ecomm/src/app/(private)/client/[userid]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserInfoInClient = () => {
+    return (
+        <>
+            <div>
+                <h1>User Info In Client Page</h1>
+            </div>
+        </>
+    )
+};
+
+export default UserInfoInClient;
