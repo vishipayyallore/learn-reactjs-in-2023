1189928d18643e23fc1cc2c838200d266487ca26
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-18 11:44:43 +0530

    Update route.ts

--- ecomm/src/app/api/users/route.ts
+++ ecomm/src/app/api/users/route.ts
@@ -3,7 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(requext: NextRequest) {
     try {
         // console.log("GET /api/users :: Request Received: ", requext);
+
         // console.log("GET /api/users :: Request Received: ", requext.nextUrl.searchParams);
+
         // requext.nextUrl.searchParams.forEach((value, key) => {
         //     console.log(`GET /api/users :: Request Received: ${key} = ${value}`);
         // });
@@ -14,6 +16,21 @@ export async function GET(requext: NextRequest) {
         const users = getUsers();
 
         return NextResponse.json({ data: users, message: "User Lists", error: null, processedAt: new Date().toUTCString() },
+            { status: 200 });
+    } catch (error: any) {
+        console.error(error);
+
+        return NextResponse.json({ data: null, message: "Something went wrong", error: error.message, processedAt: new Date().toUTCString() },
+            { status: 500 });
+    }
+}
+
+export async function POST(requext: NextRequest) {
+    try {
+        const body = await requext.json();
+        console.log("POST /api/users :: Request Received: ", body);
+
+        return NextResponse.json({ data: null, message: "User Created", error: null, processedAt: new Date().toUTCString() },
             { status: 200 });
     } catch (error: any) {
         console.error(error);
