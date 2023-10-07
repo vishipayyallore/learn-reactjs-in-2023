d091795c8e14f28a62c0311af1b75c4db167b654
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-07 16:45:53 +0530

    Update index.js

--- demo2/index.js
+++ demo2/index.js
@@ -7,22 +7,27 @@ window.onload = () => {
 
   const ints = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  const childrenElements = [];
-
-  // use map to create an array of React elements
-  ints.map((int) => {
-    childrenElements.push(React.createElement("li", { key: int }, int));
+  // this refactor is not in the clip, but would be the obvious next step
+  const childrenElements = ints.map((id, index) => {
+    return React.createElement("li", { key: ints[index] }, ints[index])
   });
-
-  // childrenElements.
-  //   push(React.createElement("li", { key: ints[0] }, ints[0]));
-
-  // childrenElements.
-  //   push(React.createElement("li", { key: ints[1] }, ints[1]));
-
-  // childrenElements.
-  //   push(React.createElement("li", { key: ints[2] }, ints[2]));
 
   root.render(childrenElements);
 
 };
+
+// const childrenElements = [];
+
+// // use map to create an array of React elements
+// ints.map((int) => {
+//   childrenElements.push(React.createElement("li", { key: int }, int));
+// });
+
+// childrenElements.
+//   push(React.createElement("li", { key: ints[0] }, ints[0]));
+
+// childrenElements.
+//   push(React.createElement("li", { key: ints[1] }, ints[1]));
+
+// childrenElements.
+//   push(React.createElement("li", { key: ints[2] }, ints[2]));
