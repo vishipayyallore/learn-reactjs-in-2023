debe246bf5704408a2ab7685cb98eb8a022dc2d4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-08 21:05:22 +0530

    Redux Provider

--- counter-with-redux/src/app/layout.tsx
+++ counter-with-redux/src/app/layout.tsx
@@ -1,7 +1,8 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
+import ReduxProvider from '@/components/ReduxProvider';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ReduxProvider>
+      {children}
+    </ReduxProvider>
   )
 }
