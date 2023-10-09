b2151eb41abf92f0b9ffd3018daabe5f07e4fd44
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-09 22:02:54 +0530

    Update page.tsx

--- counter-with-redux/src/app/page.tsx
+++ counter-with-redux/src/app/page.tsx
@@ -17,9 +17,17 @@ export default function Home() {
     dispatch(SetCountValue(count - 1));
   };
 
+  // Define a CSS class based on the count value
+  const countClass =
+    count === 0
+      ? "text-blue-500"
+      : count > 0
+        ? "text-green-500"
+        : "text-red-500";
+
   return (
     <div className="flex h-screen justify-center items-center flex-col gap-10">
-      <h1 className="text-6xl font-bold text-blue-500 text-center">Count: {count}</h1>
+      <h1 className={`text-6xl font-bold ${countClass} text-center`}>Count: {count}</h1>
 
       <div className="flex gap-5">
         <button onClick={increment} className="bg-green-700 text-white px-5 py-2 rounded-sm shadow-sm">Increment</button>
