695c1b411d536c276ea3019431c4265726eb117a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-13 11:25:08 +0530

    Update route.ts

--- taskmanager/src/app/api/users/register/route.ts
+++ taskmanager/src/app/api/users/register/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import bycrypt from "bcryptjs";
 
-import userModel from "@/models/userModel";
+import User from "@/models/userModel";
 import { connectToMongoDb } from "@/config/dbConfig";
 
 // Connect to the MongoDB before making any database operations
@@ -12,27 +12,20 @@ export async function POST(request: NextRequest) {
 
         // Verify if User exists
         const requestBody = await request.json();
-        const userExists = await userModel.exists({ email: requestBody.email });
+        const userExists = await User.exists({ email: requestBody.email });
 
         if (userExists) {
             return NextResponse.json({ error: "User already exists" }, { status: 400 });
         }
 
         // Hash the password
-        const salt = await bycrypt.genSalt(10);
-        const hashedPassword = await bycrypt.hash(requestBody.password, salt);
+        const hashedPassword = await bycrypt.hash(requestBody.password, await bycrypt.genSalt(10));
+        requestBody.password = hashedPassword;
 
-        // You should add your logic here, such as saving data to the database
-        // For example, let's assume you want to save a user
-        const user = new userModel({
-            username: "example",
-            email: "example@example.com",
-            password: "examplepassword",
-        });
+        // Save the user to the database
+        User.create(requestBody);
 
-        await user.save();
-
-        return NextResponse.json({ message: "Data saved successfully" }, { status: 200 });
+        return NextResponse.json({ message: "User registered successfully" }, { status: 200 });
     } catch (error: any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
