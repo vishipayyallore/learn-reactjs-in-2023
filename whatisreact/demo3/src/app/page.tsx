baa6b2f03f0f0a78e81b9583ac01ccfe32b466f4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-14 13:27:35 +0530

    Update page.tsx

--- whatisreact/demo3/src/app/page.tsx
+++ whatisreact/demo3/src/app/page.tsx
@@ -1,11 +1,22 @@
+'use client';
+
+import { useState } from "react";
+
 export default function Home() {
 
-  function ListItems() {
-    const ints = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const intArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  // React State
+  const [ints, setInts] = useState<number[]>(intArray);
+
+  function ListItems({ ints, addValueFunc }: { ints: number[], addValueFunc: (incrementVaule: number) => void }) {
+
+    const incrementVaule = 2;
 
     return (
       // <!-- React Fragment -->
       <>
+        <button onClick={() => addValueFunc(incrementVaule)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add New Item</button>
         {
           ints.map((id, index) => {
             return <li key={index} className="text-2xl">Item {id}</li>
@@ -15,11 +26,17 @@ export default function Home() {
     )
   }
 
+  const addValue = (incrementVaule: number) => {
+    const newInts = [...ints, ints.length + incrementVaule];
+
+    setInts(newInts);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello from ReactJS and NextJS.</h1>
       <ul className="flex flex-col items-center text-blue-800 justify-center space-y-4">
-        <ListItems />
+        <ListItems ints={ints} addValueFunc={addValue} />
       </ul>
     </main>
   )
