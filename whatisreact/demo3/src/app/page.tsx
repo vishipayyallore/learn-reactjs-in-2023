c718abe4ca58fd2fe3a40aba56348bdeeab68af7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-14 13:12:08 +0530

    Update page.tsx

--- whatisreact/demo3/src/app/page.tsx
+++ whatisreact/demo3/src/app/page.tsx
@@ -1,7 +1,26 @@
 export default function Home() {
+
+  function ListItems() {
+    const ints = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    return (
+      // <!-- React Fragment -->
+      <>
+        {
+          ints.map((id, index) => {
+            return <li key={index} className="text-2xl">Item {id}</li>
+          })
+        }
+      </>
+    )
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello from ReactJS and NextJS.</h1>
+      <ul className="flex flex-col items-center text-blue-800 justify-center space-y-4">
+        <ListItems />
+      </ul>
     </main>
   )
 }
