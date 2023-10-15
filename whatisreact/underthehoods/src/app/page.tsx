e60ed8296eba4623ac6cbc8b2fa635737beb5cde
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-15 15:17:31 +0530

    Face Lift

--- whatisreact/underthehoods/src/app/page.tsx
+++ whatisreact/underthehoods/src/app/page.tsx
@@ -8,7 +8,7 @@ const Home = () => {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-2">
-      <div className="flex flex-col items-center w-[800px] bg-slate-100 shadow-md rounded-md border-solid border-2 border-indigo-600">
+      <div className="flex flex-col items-center w-[800px] bg-slate-100 shadow-md shadow-blue-800 rounded-md border-solid border-2 border-indigo-600">
         <PageTitle title={pageTitle}></PageTitle>
 
         <ListItems numbers={numbers} />
--- whatisreact/underthehoods/src/components/ListItems.tsx
+++ whatisreact/underthehoods/src/components/ListItems.tsx
@@ -18,7 +18,7 @@ const ListItems = ({ numbers }: { numbers: number[] }) => {
     return (
         // <!-- React Fragment -->
         <>
-            <button onClick={() => addNewItem(newItemValue)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add New Item</button>
+            <button onClick={() => addNewItem(newItemValue)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded">Add New Item</button>
             <ul className="flex flex-col items-center text-blue-800 justify-center space-y-4">
                 {
                     items.map((id, index) => {
