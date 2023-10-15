78b474294879bf0da007a55b3d201b914afe842e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-15 15:05:40 +0530

    Update page.tsx

--- whatisreact/underthehoods/src/app/page.tsx
+++ whatisreact/underthehoods/src/app/page.tsx
@@ -1,48 +1,19 @@
-'use client';
-
 import ListItems from "@/components/ListItems";
 import PageTitle from "@/components/PageTitle";
-import { useState } from "react";
 
 const Home = () => {
 
   const pageTitle = 'React JS - Under the Hoods';
   const numbers = [1, 2];
 
-  // // React State
-  // const [items, setItems] = useState<number[]>(numbers);
-
-  // function ListItems({ items, addNewItemFunc }: { items: number[], addNewItemFunc: (incrementVaule: number) => void }) {
-
-  //   const incrementVaule = 1;
-
-  //   return (
-  //     // <!-- React Fragment -->
-  //     <>
-  //       <button onClick={() => addNewItemFunc(incrementVaule)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add New Item</button>
-  //       {
-  //         items.map((id, index) => {
-  //           return <li key={index} className="text-2xl">Item {id}</li>
-  //         })
-  //       }
-  //     </>
-  //   )
-  // };
-
-  // const addNewItem = (newItemVaule: number) => {
-  //   const updatedItems = [...items, items.length + newItemVaule];
-
-  //   setItems(updatedItems);
-  // };
-
   return (
-    <div>
-      <main className="flex min-h-screen flex-col items-center p-2">
+    <main className="flex min-h-screen flex-col items-center p-2">
+      <div className="flex flex-col items-center w-[800px] bg-slate-100 shadow-md rounded-md border-solid border-2 border-indigo-600">
         <PageTitle title={pageTitle}></PageTitle>
 
         <ListItems numbers={numbers} />
-      </main>
-    </div>
+      </div>
+    </main>
   )
 };
 
