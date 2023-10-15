ff9210eb1a4dc37b0a7d96a7cda447378b721203
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-15 09:45:46 +0530

    Swamy/14oct task manager day4 (#37)
    * Update index.js
    
    * Update package-lock.json
    
    * Update page.tsx
    
    * Update page.tsx
    
    * Code Refactoring
    
    * Update page.tsx
    
    * Toastor
    
    * Registration Page Integration
    
    * Update page.tsx
    
    * Create route.ts
    
    * Update route.ts
    
    * Update route.ts
    
    * Update page.tsx
    
    * Update route.ts

---
+++ taskmanager/src/app/api/users/login/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import bycrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import User from "@/models/userModel";
+import { connectToMongoDb } from "@/config/dbConfig";
+
+// Connect to the MongoDB before making any database operations
+connectToMongoDb();
+
+export const POST = async (request: NextRequest) => {
+    try {
+
+        // Verify if User exists
+        const requestBody = await request.json();
+        const userExists = await User.findOne({ email: requestBody.email });
+        if (!userExists) {
+            throw new Error("User does not exist");
+        }
+
+        const passwordMatches = await bycrypt.compare(requestBody.password, userExists.password);
+        if (!passwordMatches) {
+            throw new Error("Invalid credentials");
+        }
+
+        // Create a JWT token
+        const jwtToken = jwt.sign({ userId: userExists._id }, process.env.JWT_SECRET!, { expiresIn: "7d" });
+
+        const loginResponse = NextResponse.json({ message: "User logged in successfully" }, { status: 200 });
+        loginResponse.cookies.set("token", jwtToken, { httpOnly: true, path: "/", maxAge: 604800 }); // 7 days
+
+        return loginResponse;
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+}
