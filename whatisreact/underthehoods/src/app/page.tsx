2a02f2afa6e2880f563a87f7856c6e7fb219755b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-15 13:17:49 +0530

    Components

--- whatisreact/underthehoods/src/app/page.tsx
+++ whatisreact/underthehoods/src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import ListItems from "@/components/ListItems";
 import PageTitle from "@/components/PageTitle";
 import { useState } from "react";
 
@@ -8,40 +9,38 @@ const Home = () => {
   const pageTitle = 'React JS - Under the Hoods';
   const numbers = [1, 2];
 
-  // React State
-  const [items, setItems] = useState<number[]>(numbers);
+  // // React State
+  // const [items, setItems] = useState<number[]>(numbers);
 
-  function ListItems({ items, addNewItemFunc }: { items: number[], addNewItemFunc: (incrementVaule: number) => void }) {
+  // function ListItems({ items, addNewItemFunc }: { items: number[], addNewItemFunc: (incrementVaule: number) => void }) {
 
-    const incrementVaule = 1;
+  //   const incrementVaule = 1;
 
-    return (
-      // <!-- React Fragment -->
-      <>
-        <button onClick={() => addNewItemFunc(incrementVaule)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add New Item</button>
-        {
-          items.map((id, index) => {
-            return <li key={index} className="text-2xl">Item {id}</li>
-          })
-        }
-      </>
-    )
-  };
+  //   return (
+  //     // <!-- React Fragment -->
+  //     <>
+  //       <button onClick={() => addNewItemFunc(incrementVaule)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add New Item</button>
+  //       {
+  //         items.map((id, index) => {
+  //           return <li key={index} className="text-2xl">Item {id}</li>
+  //         })
+  //       }
+  //     </>
+  //   )
+  // };
 
-  const addNewItem = (newItemVaule: number) => {
-    const updatedItems = [...items, items.length + newItemVaule];
+  // const addNewItem = (newItemVaule: number) => {
+  //   const updatedItems = [...items, items.length + newItemVaule];
 
-    setItems(updatedItems);
-  };
+  //   setItems(updatedItems);
+  // };
 
   return (
     <div>
       <main className="flex min-h-screen flex-col items-center p-2">
         <PageTitle title={pageTitle}></PageTitle>
 
-        <ul className="flex flex-col items-center text-blue-800 justify-center space-y-4">
-          <ListItems items={items} addNewItemFunc={addNewItem} />
-        </ul>
+        <ListItems numbers={numbers} />
       </main>
     </div>
   )
