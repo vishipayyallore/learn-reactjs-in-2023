3fe050c8601251751e5a0ebcb77f6feb6036ee0a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-22 21:17:54 +0530

    Create page.tsx

---
+++ taskmanager/src/app/(private)/clienthome/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from "react";
+
+const ClientHome = () => {
+    return (
+        <div>
+            <h1>Client Home Page</h1>
+        </div>
+    )
+};
+
+export default ClientHome;
