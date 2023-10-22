0c9168852aeecdea96cbc357b7642dbdfcd0c89e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-22 20:55:52 +0530

    Update route.ts

--- taskmanager/src/app/api/users/currentuser/route.ts
+++ taskmanager/src/app/api/users/currentuser/route.ts
@@ -1,10 +1,26 @@
 import validateJwtAndGetUserId from "@/helpers/jwtValidation";
 import { NextRequest, NextResponse } from "next/server";
 
+import User from "@/models/userModel";
+import { connectToMongoDb } from "@/config/dbConfig";
+
+connectToMongoDb();
+
 export const GET = async (request: NextRequest) => {
     try {
         const userId = await validateJwtAndGetUserId(request);
 
+        if (!userId) {
+            throw new Error("Not authorized");
+        }
+
+        const user = await User.findById(userId).select("-password");
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        return NextResponse.json({ data: user, }, { status: 200 });
     } catch (error: any) {
         return NextResponse.json({ message: error.message }, { status: 500 });
     }
