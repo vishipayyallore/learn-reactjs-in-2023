a4f1e3591e8df52c6463ad452f7e9cac2a213979
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-23 11:24:07 +0530

    Header and Body

--- taskmanager/src/components/LayoutProvider.tsx
+++ taskmanager/src/components/LayoutProvider.tsx
@@ -2,13 +2,37 @@
 
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
+import { usePathname } from 'next/navigation';
 
 const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
+
+    const pathname = usePathname();
+    const isPublicRoutes = pathname === '/login' || pathname === '/register';
+
     return (
         <html lang="en">
             <body suppressHydrationWarning={true}>
                 <Toaster position='top-center' reverseOrder={false} />
-                {children}
+
+                <div className="bg-primary text-white p-3 flex items-center justify-between">
+                    <h1 className='text-xl font-semibold'>Task Manager</h1>
+
+                    <div className="flex gap-4 items-center">
+                        <h1 className='underline cursor-pointer'>User Name</h1>
+                        <i className="ri-logout-box-r-line p-2 cursor-pointer text-white"></i>
+                    </div>
+                </div>
+
+                {
+                    isPublicRoutes ?
+                        (<div>
+                            {children}
+                        </div>)
+                        :
+                        (<div className='flex px-10 py-5'>
+                            {children}
+                        </div>)
+                }
             </body>
         </html>
     );
