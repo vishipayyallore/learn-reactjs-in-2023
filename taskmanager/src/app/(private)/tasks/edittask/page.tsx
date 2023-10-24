91e4dbc69644f0e7faef6aad05df9f2e8d72bf96
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-24 19:37:12 +0530

    Update page.tsx

--- taskmanager/src/app/(private)/tasks/edittask/page.tsx
+++ taskmanager/src/app/(private)/tasks/edittask/page.tsx
@@ -1,9 +1,18 @@
+'use client';
+
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const EditTask = () => {
+    const router = useRouter();
+
     return (
-        <div>
-            <h1>Edit Task</h1>
+        <div className=" w-[100%]">
+            <div className="flex justify-between items-center w-[100%]">
+                <h1 className="text-2xl font-bold text-primary">Edit Task</h1>
+                <button className="btn-outlined rounded-sm shadow-sm"
+                    onClick={() => router.push('/tasks')}>Back</button>
+            </div>
         </div>
     );
 };
