6cf18dd6e9d0610e6912ca059e2fc6b7a2ac7469
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-24 19:02:01 +0530

    Initial Version

---
+++ taskmanager/src/app/(private)/tasks/edittask/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditTask = () => {
+    return (
+        <div>
+            <h1>Edit Task</h1>
+        </div>
+    );
+};
+
+export default EditTask;
---
+++ taskmanager/src/app/(private)/tasks/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Tasks = () => {
+    return (
+        <div>
+            <div className="flex justify-end">
+                <button className="btn bg-blue-600 text-white">Add Task</button>
+            </div>
+        </div>
+    );
+};
+
+export default Tasks;
