e815f37af1aafb798c3e184c8bc9b0d12d797679
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-24 20:58:52 +0530

    Update TaskForm.tsx

--- taskmanager/src/components/TaskForm.tsx
+++ taskmanager/src/components/TaskForm.tsx
@@ -5,7 +5,7 @@ import { TaskInterface } from "@/interfaces";
 const TaskForm = ({ task, setTask }: { task: TaskInterface, setTask: React.Dispatch<React.SetStateAction<TaskInterface>> }) => {
     return (
         <>
-            <div className="grid grid-cols-3 mt-5">
+            <div className="grid grid-cols-3 mt-5 gap-5">
                 <div className="col-span-3">
                     <label htmlFor="title" className="block text-sm font-medium text-blue-800">Title</label>
                     <input type="text" value={task.title} onChange={(e) => setTask({ ...task, title: e.target.value })} name="title" id="title" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
