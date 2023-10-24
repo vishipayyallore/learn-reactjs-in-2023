3db58099728910ccd910882d04c65288790ac720
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-24 19:16:24 +0530

    Swamy/24oct task manager day6 (#42)
    * Update Spinner.tsx
    
    * Initial Version
    
    * Update page.tsx

---
+++ taskmanager/src/app/(private)/tasks/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const Tasks = () => {
+    const router = useRouter();
+
+    return (
+        <div className=" w-[100%]">
+            <div className="flex justify-between items-center w-[100%]">
+                <h1 className="text-2xl font-bold text-primary">Tasks</h1>
+                <button className="btn bg-blue-600 text-white rounded-sm shadow-sm"
+                    onClick={() => router.push('/tasks/addtask')}>Add Task</button>
+            </div>
+        </div>
+    );
+};
+
+export default Tasks;
