f55514c3ceecd4be05345b61892808b1cfe044cc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-24 18:26:10 +0530

    Update Spinner.tsx

--- taskmanager/src/components/Spinner.tsx
+++ taskmanager/src/components/Spinner.tsx
@@ -4,7 +4,6 @@ const Spinner = () => {
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-slate-500 bg-opacity-50">
             <div className="w-16 h-16 border-8 border-white border-t-primary rounded-full animate-spin">
-
             </div>
         </div>
     );
