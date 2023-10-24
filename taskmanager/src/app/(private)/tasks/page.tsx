97342d9b76d903a1018937247addf41024926c3f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-24 19:13:23 +0530

    Update page.tsx

--- taskmanager/src/app/(private)/tasks/page.tsx
+++ taskmanager/src/app/(private)/tasks/page.tsx
@@ -1,10 +1,17 @@
+'use client';
+
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Tasks = () => {
+    const router = useRouter();
+
     return (
-        <div>
-            <div className="flex justify-end">
-                <button className="btn bg-blue-600 text-white">Add Task</button>
+        <div className=" w-[100%]">
+            <div className="flex justify-between items-center w-[100%]">
+                <h1 className="text-2xl font-bold text-primary">Tasks</h1>
+                <button className="btn bg-blue-600 text-white rounded-sm shadow-sm"
+                    onClick={() => router.push('/tasks/addtask')}>Add Task</button>
             </div>
         </div>
     );
