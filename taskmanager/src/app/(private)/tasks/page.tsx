86184bd7c3c83f7f565aa5ea83c13192905b1728
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-25 13:02:28 +0530

    Changes

--- taskmanager/src/app/(private)/tasks/page.tsx
+++ taskmanager/src/app/(private)/tasks/page.tsx
@@ -1,20 +1,31 @@
-'use client';
+// 'use client';
 
-import { useRouter } from "next/navigation";
 import React from "react";
+import Link from "next/link";
+// import { useRouter } from "next/navigation";
 
 const Tasks = () => {
-    const router = useRouter();
+    // const router = useRouter();
 
     return (
         <div className=" w-[100%]">
             <div className="flex justify-between items-center w-[100%]">
                 <h1 className="text-2xl font-bold text-primary">Tasks</h1>
-                <button className="btn-primary rounded-sm shadow-sm"
-                    onClick={() => router.push('/tasks/addtask')}>Add Task</button>
+                <button className="btn-primary rounded-sm shadow-sm">
+                    <Link href="/tasks/addtask">Add Task</Link>
+                </button>
             </div>
         </div>
     );
 };
 
 export default Tasks;
+
+
+{/* <div className=" w-[100%]">
+    <div className="flex justify-between items-center w-[100%]">
+        <h1 className="text-2xl font-bold text-primary">Tasks</h1>
+        <button className="btn-primary rounded-sm shadow-sm"
+            onClick={() => router.push('/tasks/addtask')}>Add Task</button>
+    </div>
+</div> */}
