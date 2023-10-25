4a8ed6ecfe6d70d5d63a5f94a738569e8aa832d3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-25 13:15:26 +0530

    Update page.tsx

--- taskmanager/src/app/(private)/tasks/page.tsx
+++ taskmanager/src/app/(private)/tasks/page.tsx
@@ -1,26 +1,55 @@
-// 'use client';
-
 import React from "react";
 import Link from "next/link";
-// import { useRouter } from "next/navigation";
+import { cookies } from "next/headers";
+import axios from "axios";
 
-const Tasks = () => {
-    // const router = useRouter();
+const getTasks = async () => {
+    try {
+        const cookieStore = cookies();
+        const token = cookieStore.get("token")?.value;
+
+        const endpoint = `${process.env.API_URL}/tasks`;
+        const response = await axios.get(endpoint, {
+            headers: {
+                "Cookie": `token=${token}`,
+            },
+        });
+
+        console.log('getTasks :', response.data);
+        return response.data;
+
+    } catch (error: any) {
+        console.log(error.message);
+        return [];
+    }
+};
+
+const Tasks = async () => {
+
+    const tasks: any = await getTasks();
 
     return (
         <div className=" w-[100%]">
             <div className="flex justify-between items-center w-[100%]">
                 <h1 className="text-2xl font-bold text-primary">Tasks</h1>
                 <button className="btn-primary rounded-sm shadow-sm">
-                    <Link href="/tasks/addtask">Add Task</Link>
+                    <Link href="/tasks/addtask">New Task</Link>
                 </button>
             </div>
+
+            {
+                tasks?.length
+            }
         </div>
     );
 };
 
 export default Tasks;
 
+// 'use client';
+
+// import { useRouter } from "next/navigation";
+// const router = useRouter();
 
 {/* <div className=" w-[100%]">
     <div className="flex justify-between items-center w-[100%]">
