69d164d759c46b285ec83178359063b2effb6858
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-27 10:38:54 +0530

    Update route.ts

--- taskmanager/src/app/api/dashboard/route.ts
+++ taskmanager/src/app/api/dashboard/route.ts
@@ -1,8 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req: NextRequest) => {
-    try {
+import validateJwtAndGetUserId from "@/helpers/jwtValidation";
+import Task from "@/models/taskModel";
+import { connectToMongoDb } from "@/config/dbConfig";
 
+connectToMongoDb();
+
+export const GET = async (request: NextRequest) => {
+    try {
+        const userId = await validateJwtAndGetUserId(request);
+        if (!userId) {
+            return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+        }
+
+        const tasks = await Task.find({ user: userId }).sort({ createdAt: -1 });
+
+        return NextResponse.json(tasks, { status: 200 });
     } catch (error: any) {
         console.log(error);
         return NextResponse.json({ error: error.message }, { status: 500 });
