9c4217e9fa2e1db3b9957f7c61c33a58ecddd1fb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-28 14:42:33 +0530

    Update ClearFilters.tsx

--- taskmanager/src/app/(private)/tasks/_components/ClearFilters.tsx
+++ taskmanager/src/app/(private)/tasks/_components/ClearFilters.tsx
@@ -1,16 +1,22 @@
 'use client';
 
 import React from "react";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const ClearFilters = () => {
 
     const searchParams = useSearchParams();
+    const router = useRouter();
+
+    const clearFilters = () => {
+        router.refresh();
+        router.push('/tasks');
+    };
 
     if (searchParams.get('status') || searchParams.get('priority')) {
         return (
             <div>
-                <button className="bg-red-500 hover:bg-red-700 text-white border border-red-900 rounded-sm shadow-sm">
+                <button className="bg-red-500 hover:bg-red-700 text-white border border-red-900 rounded-sm shadow-sm" onClick={clearFilters}>
                     Clear Filters
                 </button>
             </div>
