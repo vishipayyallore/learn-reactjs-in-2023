81ff036772703a83dfa51f4b97ff8251967a1793
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-29 13:11:44 +0530

    Create page.jsx

---
+++ game-reviews/app/reviews/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ReviewsPage = () => (
+    <>
+        <h1>Reviews</h1>
+        <p>Here we will list all the reviews.</p>
+    </>
+);
+
+export default ReviewsPage;
