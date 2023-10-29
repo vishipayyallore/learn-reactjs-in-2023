e54432735b20e3e580e95069cac1bf8f13f520f2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-29 13:21:57 +0530

    Changes

---
+++ game-reviews/app/about/page.jsx
@@ -0,0 +1,21 @@
+// import Heading from '@/components/Heading';
+
+import React from "react";
+
+export const metadata = {
+    title: 'About',
+};
+
+const AboutPage = () => {
+    return (
+        <>
+            {/* <Heading>About</Heading> */}
+            <h1>About</h1>
+            <p>
+                A website created to learn Next.js
+            </p>
+        </>
+    );
+};
+
+export default AboutPage;
