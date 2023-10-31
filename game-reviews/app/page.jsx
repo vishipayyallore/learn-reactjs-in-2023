71fef2af3a2d283d0b5a61ac02d49f7580263585
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-31 07:47:42 +0530

    Swamy/29oct nextjs by example day1 (#54)
    * Initial Version
    
    * Renamed the file
    
    * Update Layout.jsx
    
    * Rename Layout.jsx to layout.jsx
    
    * Create page.jsx
    
    * Create page.jsx
    
    * Changes
    
    * Update page.jsx

--- game-reviews/app/page.jsx
+++ game-reviews/app/page.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const HomePage = () => (
+
+    console.log("Home Page", new Date().toUTCString()),
+    
     <>
         <h1>Indie Gamer</h1>
         <p>Only the best Indie Games, reviewed for you.</p>
