313e6c697fe14644ce91c03acda2e3246c21e637
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-07 10:05:08 +0530

    Initial Version

---
+++ ecomm/src/app/(private)/countries/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import React from "react";
+
+import PhotoList from "@/components/PhotoList";
+import './PhotoCard.css';
+
+const PhotosPage = () => {
+
+  const [photos, setPhotos] = React.useState([]);
+
+  React.useEffect(() => {
+    getphotos();
+  }, []);
+
+  const getphotos = async () => {
+    try {
+      await fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(response => response.json())
+        .then(data => setPhotos(data))
+    } catch (error: any) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl font-bold text-center text-cyan-900">Photo Gallery</h1>
+        <hr></hr>
+
+        <div>
+          <PhotoList photos={photos.slice(0, 6)} />
+        </div>
+
+      </div>
+    </>
+  )
+};
+
+export default PhotosPage;
